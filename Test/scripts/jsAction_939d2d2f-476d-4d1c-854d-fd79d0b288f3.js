1c272bc2855689a211151d575bcd98275440b86c
mndiayegamman <mansour.ndiaye@gamman.fr>
2023-09-30 01:43:36 +0200

    Neoload test jpetstore

---
+++ Test/scripts/jsAction_939d2d2f-476d-4d1c-854d-fd79d0b288f3.js
@@ -0,0 +1,7 @@
+﻿// Javascript skeleton.
+// Edit and adapt to your needs.
+// The documentation of the NeoLoad Javascript API
+// is available in the appendix of the documentation.
+
+logger.debug("CategoryId is ",$categoryId);
+
